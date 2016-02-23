58888064f3525c712c2057f93d498adf86dd4517
Georgi <Georgi@Georgi-PC>
2016-02-24 00:27:56 +0200

    31, 1-17. Write a program to print all input lines that are longer than 80 characters

--- main.c
+++ main.c
@@ -1,41 +1,31 @@
-/* 30, 1-16. Revise the main routine of the longest-line program so it will 
- * correctly print the length of arbitrarily long input lines, and as 
- * much as possible of the text */
+/* 31, 1-17. Write a program to print all input lines that are longer than
+ * 80 characters */
 
 #include <stdio.h>
 #define MAXLINE 1000    // maximum input line size
+#define LONGLINE 80
 
 int get_line(char line[], int maxline);
-void copy(char to[], char from[]);
 
-// print longest input line
+// print lines longer than LONGLINE
 int main() {
     int len;                // current line lenght
-    int max;                 // maximum length seen so far
     char line[MAXLINE];     // current input line
-    char longest[MAXLINE];  // longest line saved here
     
-    max = 0;
-    while ((len = get_line(line, MAXLINE)) > 0) {
-        printf("%d, %s", len, line);
-        if (len > max) {
-            max = len;
-            copy(longest, line);
-        }
-    }
-    if (max > 0)            // there was a line
-        printf("%s", longest);
+    while ((len = get_line(line, MAXLINE)) > 0)
+        if (len > LONGLINE)
+            printf("%s", line);
     return 0;
 }
 
-// get)line: read a line into s, return length
+// get_line: read a line into s, return length
 int get_line(char s[], int lim) {
     int c, i, j;
     
     j = 0;
     for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i)
-        if (i < lim-2) {
-            s[j] = c;       // line still in boundaries
+        if(i < lim - 2) {
+            s[j] = c;           // line still in boundaries
             ++j;
         }
     if (c == '\n') {
@@ -47,11 +37,3 @@ int get_line(char s[], int lim) {
     return i;
 }
 
-// copy: copy 'from'into 'to'; assume to is big enough
-void copy(char to[], char from[]) {
-    int i;
-    
-    i = 0;
-    while ((to[i] = from[i]) != '\0')
-        ++i;
-}
