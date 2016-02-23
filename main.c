bf3f6390ace05828137efca0cf7f230e41a42307
Georgi <Georgi@Georgi-PC>
2016-02-23 10:09:26 +0200

    24, 1-14. Write a program to print a histogram of the frequencies of different characters in its input.

--- main.c
+++ main.c
@@ -1,60 +1,43 @@
-/* 24, 1-13,  Write a program to print a histogram of the lengths of words in
-its input. It is easy to draw the histogram with the bars horizontal; a vertical
-orientation is more challenging. Vertical histogram */
-#include <stdio.h>    
-#define MAXHIST 15  // max lenght of histogram 
-#define MAXWORD 11  // max length of a word
-#define IN 1        // inside a word
-#define OUT 0       // outside a word
+/* 24, 1-14. Write a program to print a histogram of the frequencies of 
+ * different characters in its input. */
+#include <stdio.h>
+#include <ctype.h>
 
-//printf vertical histogram
+#define MAXHIST 15          // max length of histogram
+#define MAXCHAR 128         // max different characters
+
+// printf horizontal histogram freq. of different characters
 int main() {
-    int c, i, j, nc, state;
-    int maxvalue;           // maximum value for wl[]
-    int ovflow;             // number of overflow words
-    int wl[MAXWORD];        // word length counters
+    int c, i;
+    int len;            // length of each bar
+    int maxvalue;       // maximum value for cc[]
+    int cc[MAXCHAR];    // character counters
     
-    state = OUT;
-    nc = 0;         // number of characters in a word
-    ovflow = 0;     // number of words >= MAXWORD
-    for (i = 0; i < MAXWORD; ++i)
-        wl[i] = 0;
-    while ((c = getchar()) != EOF) {
-        if (c == ' ' || c == '\n' || c == '\t') {
-            state = OUT;
-            if (nc > 0)
-                if (nc < MAXWORD)
-                    ++wl[nc];
-                else
-                    ++ovflow;
-            nc = 0;
-        } else if (state == OUT) {
-            state = IN;
-            nc = 1;         // beginning of a new word
-        } else
-            ++nc;           // inside a word
-    }
+    for (i = 0; i < MAXCHAR; ++i)
+        cc[i] = 0;
+    while ((c = getchar()) != EOF)
+        ++cc[c];
     maxvalue = 0;
-    for (i = 1; i < MAXWORD; ++i)
-        if (wl[i] > maxvalue)
-            maxvalue = wl[i];
+    for (i = 1; i < MAXCHAR; ++i)
+        if (cc[i] > maxvalue)
+            maxvalue = cc[i];
     
-    for (i = MAXHIST; i > 0; --i) {
-        for (j = 1; j < MAXWORD; ++j)
-            if (wl[j] * MAXHIST / maxvalue >= i)
-                printf(" * ");
-            else
-                printf("   ");
+    for (i = 1; i < MAXCHAR; ++i) {
+        if (isprint(i))
+            printf("%5d - %c - %5d : ", i, i, cc[i]);
+        else
+            printf("%5d -   - %5d : ", i, cc[i]);
+        if (cc[i] > 0) {
+            if ((len = cc[i] * MAXHIST / maxvalue) <= 0)
+                len = 1;
+        } else
+            len = 0;
+        while (len > 0) {
+            putchar('*');
+            --len;
+        }
         putchar('\n');
     }
-    for (i = 1; i < MAXWORD; ++i)
-        printf("%4d ", i);
-    putchar('\n');
-    for (i = 1; i < MAXWORD; ++i)
-        printf("%4d ", wl[i]);
-    putchar('\n');
-    if (ovflow > 0)
-        printf("There are %d words >= %d\n", ovflow, MAXWORD);
     return 0;
 }
 
