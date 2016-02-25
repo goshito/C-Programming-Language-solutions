84dadb8239e096a0d416989449b1d7a9c4e827b8
Georgi <Georgi@Georgi-PC>
2016-02-25 03:52:16 +0200

    31, Exercise 1-19, Write a function reverse(s) that reverses the character  string s. Use it to write a program that reverses its input a line at  a time

--- main.c
+++ main.c
@@ -1,49 +1,55 @@
-/* 31, Exercise 1-18. Write program to remove trailing blanks and tabs from
- each line of input, and to delete entirely blank lines */
+/* 
+ * 31, Exercise 1-19, Write a function reverse(s) that reverses the character
+ * string s. Use it to write a program that reverses its input a line at 
+ * a time
+ */
 
 #include <stdio.h>
-#define MAXLINE 1000    // maximum input line size
+#define MAXLINE 1000        //maximum input line size
 
 int get_line(char line[], int maxline);
-int remove_(char s[]);
+void reverse(char s[]);
 
-// remove trailing blanks and tabs, and delete blank lines
+//reverse input lines, a line at a time
 int main() {
     char line[MAXLINE];     // current input line
     
-    while (get_line(line, MAXLINE) > 0)
-        if (remove(line) > 0)
-            printf("%s", line);
+    while(get_line(line, MAXLINE) > 0) {
+        reverse(line);
+        printf("%s", line);
+    }
     return 0;
 }
 
-/* remove trailing blanks and tabs from character string s */
-int remove_(char s[]) {
-    int i;
+// reverse: reverse string s
+void reverse(char s[]) {
+    int i, j;
+    char temp;
     
     i = 0;
-    while (s[i] != '\n')        //find newline character
+    while(s[i] != '\0') // find the end of string s
         ++i;
-    --i;                        // back off from '\n'
-    while (i >= 0 && (s[i] == ' ' || s[i] == '\t'))
+    --i;                // back off from '\0'
+    if(s[i] == '\n')
+        --i;            // leave newline in place
+    j = 0;
+    while (j < i) {
+        temp = s[j];
+        s[j] = s[i];    // swap the characters
+        s[i] = temp;
         --i;
-    if (i >= 0) {               // is it a nonblank line?
-        ++i;
-        s[i] = '\n';            // put newline character back
-        ++i;
-        s[i] = '\0';            // terminate the string
+        --j;
     }
-    return i;
 }
 
-// get_line: read a line into s, return length
+// getline: read a line into s, return length
 int get_line(char s[], int lim) {
     int c, i, j;
     
     j = 0;
     for (i = 0; (c = getchar()) != EOF && c != '\n'; ++i)
         if (i < lim - 2) {
-            s[j] = c;           // line still in boundaries
+            s[j] = c;   // line still in boundaries
             ++j;
         }
     if (c == '\n') {
