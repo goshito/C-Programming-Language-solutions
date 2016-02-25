106d4de4ffd0cace5d98bc48bbadf7199cbbe7f4
Georgi <Georgi@Georgi-PC>
2016-02-25 06:16:36 +0200

    34, 1-21, Write a program entab that replaces strings of blanks by the minimum number  of tabs and blanks to achieve the same spacing. Use the same tab stops as  for detab. When either a tab or a single blank would suffice to reach a tab  stop, which should be given preference?

--- main.c
+++ main.c
@@ -1,33 +1,38 @@
-/* 34, Ex. 1-20 Write a program detab that replaces tabs in the input with the 
- * proper number of blanks to space to the next tab stop. Assume a fixed set
- * of tab stops, say every n columns. Should n be a variable or a symbolic
- * parameter? */
+/* 34, 1-21, Write a program entab that replaces strings of blanks by the minimum number
+ of tabs and blanks to achieve the same spacing. Use the same tab stops as
+ for detab. When either a tab or a single blank would suffice to reach a tab
+ stop, which should be given preference? */
 
 #include <stdio.h>
 
-#define TABINC 8   // tab increment size
+#define TABINC 8                // tab increment size
 
-// replace tabs with the proper number of blanks
+// replace strings of blanks with tabs and blanks
 int main() {
-    int c, nb, pos;
+    int c, nb, nt, pos;
     
-    nb = 0;         // number of blanks necessary
-    pos = 1;        // position of the character in line
-    while ((c = getchar()) != EOF) {
-        if (c == '\t') {        // tab character
-            nb =  TABINC - (pos - 1) % TABINC;
-            while (nb > 0) {
-                putchar(' ');
-                ++pos;
-                --nb;
+    nb = 0;                     // # of blanks necessary
+    nt = 0;                     // # of tabs necessary
+    for (pos = 1; (c = getchar()) != EOF; ++pos)
+        if (c == ' ') {
+            if (pos % TABINC != 0)
+                ++nb;           // increment # of blanks
+            else {
+                nb = 0;         // reset # of blanks
+                ++nt;           // one more tab
             }
-        } else if (c == '\n') { // newline character
+        } else {
+            for ( ; nt > 0; --nt)
+                putchar('\t');  // output tab(s)
+            if (c == '\t')      // forget the blank(s)
+                nb = 0;
+            else                // output blanks
+                for ( ; nb > 0; --nb)
+                    putchar(' ');
             putchar(c);
-            pos = 1;
-        } else {                // all other characters
-            putchar(c);
-            ++pos;
+            if (c == '\n')
+                pos = 0;
+            else if (c == '\t')
+                pos = pos + (TABINC - (pos - 1) % TABINC) - 1;
         }
-    }
 }
- 
